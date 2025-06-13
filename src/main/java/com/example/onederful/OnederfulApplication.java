dcbc4ba4983d9477d8bd43b04f05889b0dc59788
leeuihyun <meta212121@naver.com>
2025-06-13 15:05:17 +0900

    feat : SpringBoot Project 초기 설정

---
+++ src/main/java/com/example/onederful/OnederfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.onederful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnederfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnederfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onederful/OnederfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onederful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnederfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
