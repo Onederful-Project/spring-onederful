d72a7daded3786078d71698a85208b96067db3b7
leeuihyun <meta212121@naver.com>
2025-06-17 15:55:20 +0900

    feat : 요구사항 변경에 따른 응답 DTO 수정

--- src/main/java/com/example/onederful/domain/task/dto/response/TaskResponse.java
+++ src/main/java/com/example/onederful/domain/task/dto/response/TaskResponse.java
@@ -1,6 +1,7 @@
 package com.example.onederful.domain.task.dto.response;
 
 import com.example.onederful.domain.task.entity.Task;
+import com.example.onederful.domain.task.enums.Priority;
 import com.example.onederful.domain.task.enums.ProcessStatus;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
@@ -19,9 +20,10 @@ public class TaskResponse {
     private Long id;
     private String title;
     private String description;
-    private String priority;
+    private Priority priority;
     private ProcessStatus status;
-    private String managerName;
+    private Long assigneeId;
+    private TaskAssignee assignee;
 
     private OffsetDateTime dueDate;
     private OffsetDateTime createdAt;
@@ -33,8 +35,9 @@ public class TaskResponse {
             .title(task.getTitle())
             .description(task.getDescription())
             .status(task.getStatus())
-            .priority(task.getPriority().name())
-            .managerName(task.getManager().getName())
+            .priority(task.getPriority())
+            .assigneeId(task.getAssignee().getId())
+            .assignee(TaskAssignee.of(task.getAssignee()))
             .dueDate(task.getDueDate().atOffset(ZoneOffset.UTC))
             .createdAt(task.getCreatedAt().atOffset(ZoneOffset.UTC))
             .updatedAt(task.getUpdatedAt().atOffset(ZoneOffset.UTC))
