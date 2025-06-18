eedbad840911a97d3df489053abd0402246771a3
DongGeun <kk506kk@naver.com>
2025-06-18 19:50:20 +0900

    refactor : 새로운 에러코드 생성

--- src/main/java/com/example/onederful/exception/ErrorCode.java
+++ src/main/java/com/example/onederful/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     // User
-    DUPLICATE_USER(HttpStatus.CONFLICT,"이미 존재하는 사용자입니다."),
+    DUPLICATE_USER(HttpStatus.CONFLICT,"이미 존재하는 사용자명입니다."),
+    DUPLICATE_EMAIL(HttpStatus.CONFLICT,"이미 존재하는 이메일입니다."),
     BAD_REQUEST(HttpStatus.BAD_REQUEST,"잘못된 사용자명 또는 비밀번호입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"인증이 필요합니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다."),
