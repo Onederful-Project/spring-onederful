9b487d7e58145f7d3affc0b27e787505fe6974b6
DongGeun <kk506kk@naver.com>
2025-06-18 10:16:40 +0900

    feat : jwt 예외 처리 중 완성

--- src/main/java/com/example/onederful/filter/JwtFilter.java
+++ src/main/java/com/example/onederful/filter/JwtFilter.java
@@ -1,19 +1,11 @@
 package com.example.onederful.filter;
 
 import com.example.onederful.security.JwtUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SecurityException;
-import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import java.io.IOException;
 import java.time.OffsetDateTime;
@@ -52,27 +44,20 @@ public class JwtFilter implements Filter {
 
         // "Bearer" 빼고 확인
         String jwt = authorizationHeader.substring(7);
-        
-        // 토큰 검증 유무 확인 (응답 형태가 올바르지 못한 상태)
-        try {
-            jwtUtil.validateToken(jwt);
+
+//        // 토큰 검증
+        String errorMessage = jwtUtil.validateToken(jwt);
+        if (errorMessage != null) {
+            errorResponse(response, HttpServletResponse.SC_FORBIDDEN, errorMessage);
+            return;
         }
-        catch (SignatureException e) {
-            errorResponse(response, HttpServletResponse.SC_FORBIDDEN, "유효하지 않은 JWT 서명입니다.");
-        } catch (SecurityException | MalformedJwtException e){
-            errorResponse(response,HttpServletResponse.SC_FORBIDDEN,"잘못된 JWT 토큰 형식입니다.");
-        }catch (ExpiredJwtException e){
-            errorResponse(response,HttpServletResponse.SC_FORBIDDEN,"Expired JWT token, 만료된 JWT token 입니다.");
-        }catch (UnsupportedJwtException e){
-            errorResponse(response,HttpServletResponse.SC_FORBIDDEN,"Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
-        }catch (IllegalArgumentException e){
-            errorResponse(response,HttpServletResponse.SC_FORBIDDEN,"JWT claims is empty, 잘못된 JWT 토큰 입니다.");
-        }
+
 
 
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
+    // 공통 에러 응답 처리
     private void errorResponse(HttpServletResponse response, int status, String message) throws IOException {
         response.setStatus(status);
         response.setContentType("application/json;charset=utf-8");
@@ -87,11 +72,4 @@ public class JwtFilter implements Filter {
         response.getWriter().write(json);
     }
 
-//    private void errorResponse(HttpServletResponse response, String message) throws IOException{
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        response.setCharacterEncoding("utf-8");
-//        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-//        response.getWriter().write(objectMapper.writeValueAsString(Response.builder));
-//    }
 }
