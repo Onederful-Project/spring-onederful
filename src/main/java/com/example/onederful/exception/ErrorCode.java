cd5fcfc6d46e8eb7604ae97c20e36616d31c7b7f
kimminsung <shiwing15@naver.com>
2025-06-18 12:00:27 +0900

    feat: UserNotFound 에러코드 추가

--- src/main/java/com/example/onederful/exception/ErrorCode.java
+++ src/main/java/com/example/onederful/exception/ErrorCode.java
@@ -11,7 +11,9 @@ public enum ErrorCode {
     BAD_REQUEST(HttpStatus.BAD_REQUEST,"잘못된 사용자명 또는 비밀번호입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED,"인증이 필요합니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"비밀번호가 일치하지 않습니다."),
-    LOGOUT_FAIL(HttpStatus.UNAUTHORIZED,"로그아웃에 실패하였습니다.");
+    LOGOUT_FAIL(HttpStatus.UNAUTHORIZED,"로그아웃에 실패하였습니다."),
+
+    USER_NOT_EXIST(HttpStatus.NOT_FOUND, "존재하지 않는 유저입니다.");
 
     private final HttpStatus status;
     private final String message;
