88c74463af0a8bd48de5eb9eded04de8fd7dd5c5
DongGeun <kk506kk@naver.com>
2025-06-19 10:10:45 +0900

    feat : 모든 회원 조회 기능 추가

--- src/main/java/com/example/onederful/domain/user/controller/AuthController.java
+++ src/main/java/com/example/onederful/domain/user/controller/AuthController.java
@@ -4,14 +4,18 @@ import com.example.onederful.common.ApiResponseDto;
 import com.example.onederful.domain.user.common.LoginGroup;
 import com.example.onederful.domain.user.common.PasswordGroup;
 import com.example.onederful.domain.user.common.SignupGroup;
+import com.example.onederful.domain.user.common.UserMapper;
 import com.example.onederful.domain.user.dto.*;
 import com.example.onederful.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.RequestInfo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,10 +23,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    
+
     // 회원가입
     @PostMapping("/auth/register")
-    public ResponseEntity<ApiResponseDto> register(@Validated(SignupGroup.class) @RequestBody RequestDto requestDto){
+    public ResponseEntity<ApiResponseDto> register(@Validated(SignupGroup.class)  @RequestBody RequestDto requestDto){
 
         UserResponseDto signup = userService.signup(requestDto);
 
@@ -33,7 +37,7 @@ public class AuthController {
 
     // 로그인
     @PostMapping("/auth/login")
-    public ResponseEntity<ApiResponseDto> login(@Validated(LoginGroup.class) @RequestBody RequestDto requestDto){
+    public ResponseEntity<ApiResponseDto> login(@Validated(LoginGroup.class)  @RequestBody RequestDto requestDto){
 
         Tokeninfo token = userService.login(requestDto);
 
@@ -56,7 +60,7 @@ public class AuthController {
     // 회원 탈퇴 (계정 삭제)
     @PostMapping("/auth/withdraw")
     public ResponseEntity<ApiResponseDto> withdraw (HttpServletRequest request,
-                                                    @Validated(PasswordGroup.class) @RequestBody RequestDto dto){
+                                                    @Validated(PasswordGroup.class)  @RequestBody RequestDto dto){
 
         userService.withdraw(request,dto);
 
@@ -64,6 +68,17 @@ public class AuthController {
 
         return ResponseEntity.status(HttpStatus.OK).body(success);
     }
+
+    // 모든 회원 정보 조회
+    @GetMapping("/users")
+    public ResponseEntity<ApiResponseDto> selectAll(){
+        List<UserResponseDto> selectAll = userService.selectAll();
+
+        ApiResponseDto success = ApiResponseDto.success("요청이 성공적으로 처리되었습니다.",selectAll);
+
+        return ResponseEntity.status(HttpStatus.OK).body(success);
+    }
+
 
 
 }
--- src/main/java/com/example/onederful/domain/user/service/UserService.java
+++ src/main/java/com/example/onederful/domain/user/service/UserService.java
@@ -15,6 +15,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -23,8 +26,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
-    @Transactional
+
     // 회원가입
+    @Transactional
     public UserResponseDto signup(RequestDto dto){
         
         // 이메일 중복 확인
@@ -72,8 +76,9 @@ public class UserService {
         return token(token);
     }
 
-    @Transactional
+
     // 회원 정보 조회
+    @Transactional
     public UserResponseDto select(HttpServletRequest request){
 
         // 토큰에서 Id 가져오기
@@ -86,8 +91,9 @@ public class UserService {
         return UserMapper.data(user);
     }
 
-    @Transactional
+
     // 회원 탈퇴
+    @Transactional
     public void withdraw(HttpServletRequest request , RequestDto dto){
         // 토큰에서 Id 가져오기
         Long userId = jwtUtil.extractId(request);
@@ -105,6 +111,24 @@ public class UserService {
 
         user.delete();
     }
+
+
+    // 모든 회원 정보 조회
+    public List<UserResponseDto> selectAll(){
+
+        List<UserResponseDto> result = new ArrayList<>();
+
+        List<User> all = userRepository.findAll();
+
+        for(User user : all){
+            result.add(UserMapper.data(user));
+        }
+
+        return result;
+
+    }
+
+
 
 
     // ResponseBody date (Token)
